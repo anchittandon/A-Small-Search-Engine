f6ec096e11251a6f52d0c2e07722dfe501d261a1
Anchit Tandon <mt6170772@iitd.ac.in>
2018-10-14 02:51:55 +0530

    Added InvertedPageIndex.java

---
+++ InvertedPageIndex.java
@@ -0,0 +1,14 @@
+interface InvertedPageIndexInterface{
+	void addPage(PageEntry p);
+	MySet<PageEntry> getPagesWhichContainWord(String str);
+}
+
+
+public class InvertedPageIndex implements InvertedPageIndexInterface{
+	void addPage(PageEntry p){
+
+	}
+	MySet<PageEntry> getPagesWhichContainWord(String str){
+	
+	}
+}
