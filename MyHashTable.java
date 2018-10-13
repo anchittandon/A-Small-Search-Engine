804303188bf94ee11fab94c04ef6f6c23c15fb0a
Anchit Tandon <mt6170772@iitd.ac.in>
2018-10-14 02:51:08 +0530

    Added MyHashTable.java

---
+++ MyHashTable.java
@@ -0,0 +1,13 @@
+interface MyHashTableInterface{
+	private int getHashIndex(String str);
+	void addPositionsForWord(WordEntry w);
+}
+
+public class MyHashTable implements MyHashTableInterface{
+	private int getHashIndex(String str){
+
+	}
+	void addPositionsForWord(WordEntry w){
+	
+	}
+}
