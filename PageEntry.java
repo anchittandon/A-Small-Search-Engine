ad6b1907bf6f2fcf1b465e87c59f402ed6cc70df
Anchit Tandon <mt6170772@iitd.ac.in>
2018-10-14 02:50:34 +0530

    Added PageEntry.java

---
+++ PageEntry.java
@@ -0,0 +1,14 @@
+interface PageEntryInterface{
+	PageEntry(String pageName);
+	PageIndex getPageIndex();
+}
+
+public class PageEntry implements PageEntryInterface{
+	PageEntry(String pageName){
+		
+	}
+	PageIndex getPageIndex(){
+		
+	}
+
+}
