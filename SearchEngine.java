74c691b806b6b1ac608cff5adb4f87e7ecd75024
Anchit Tandon <mt6170772@iitd.ac.in>
2018-10-14 02:52:26 +0530

    Added SearchEngine.java

---
+++ SearchEngine.java
@@ -0,0 +1,13 @@
+interface SearchEngineInterface{
+	SearchEngine();
+	void performAction(String actionMessage);
+}
+
+public class SearchEngine implements SearchEngineInterface{
+	SearchEngine(){
+
+	}
+	void performAction(String actionMessage){
+	
+	}
+}
