d613991c7d0eedb0201bb40fc56c9c66f7859fc7
Anchit Tandon <mt6170772@iitd.ac.in>
2018-10-15 16:11:46 +0530

    Modified Myset.java
    Added generics to MysetInterface
    Added generics to MyLinkedList object declarations
    Replaced head with getHead()
    Added find() method

--- Myset.java
+++ Myset.java
@@ -1,19 +1,19 @@
-interface MysetInterface{
+interface MysetInterface<T>{
 	public Boolean IsEmpty();
-	public Boolean IsMember(Object o);
-	public void Insert(Object o);
-	public void Delete(Object o) throws Exception;
-	public Myset Union(Myset a);
-	public Myset Intersection(Myset a);
+	public Boolean IsMember(T o);
+	public void Insert(T o);
+	public void Delete(T o) throws Exception;
+	public Myset<T> Union(Myset<T> a);
+	public Myset<T> Intersection(Myset<T> a);
 }
 
 
-public class Myset<T> implements MysetInterface{
+public class Myset<T> implements MysetInterface<T>{
 
 	MyLinkedList<T> myset;
 
 	Myset(){
-		myset = new MyLinkedList ();
+		myset = new MyLinkedList<T>();
 	}
 	public Boolean IsEmpty(){
 		//Returns true if the set is empty
@@ -22,7 +22,7 @@ public class Myset<T> implements MysetInterface{
 	public Boolean IsMember(T o){
 		//O(n)
 		//Returns true if o is in the set, false otherwise.
-		MyLinkedList.Node itr= myset.head;
+		MyLinkedList<T>.Node itr= myset.getHead();
 		while(itr!=null){
 			//System.out.println(itr.getData());
 			if(itr.getData().equals(o)){
@@ -47,8 +47,8 @@ public class Myset<T> implements MysetInterface{
 				myset.remove();
 			}
 			else{
-				MyLinkedList.Node prev = myset.head;
-				MyLinkedList.Node itr = myset.head.getNext();
+				MyLinkedList<T>.Node prev = myset.head;
+				MyLinkedList<T>.Node itr = myset.head.getNext();
 				while(itr!=null){
 					if(itr.getData().equals(o)){
 						prev.next = itr.next;
@@ -62,27 +62,28 @@ public class Myset<T> implements MysetInterface{
 			throw new NotInSetException(o+" is not in the set.");
 		}
 	}
-	public Myset Union(Myset a){
+
+	public Myset<T> Union(Myset<T> a){
 		//O(n^2)
 		//Returns a set which is the union of the current set with the set a
-		Myset answer = new Myset();
-		MyLinkedList.Node itr = myset.head;
+		Myset<T> answer = new Myset<T>();
+		MyLinkedList<T>.Node itr = myset.getHead();
 		while(itr!=null){
 			answer.Insert(itr.getData());
 			itr= itr.getNext();
 		}
-		itr = a.myset.head;
+		itr = a.myset.getHead();
 		while(itr!=null){
 			answer.Insert(itr.getData());
 			itr = itr.getNext();
 		}
 		return answer;
 	}
-	public Myset Intersection(Myset a){
+	public Myset<T> Intersection(Myset<T> a){
 		//O(n^2)
 		//Returns a set which is the intersection of the current set with the set a
-		Myset answer = new Myset();
-		MyLinkedList.Node itr = myset.head;
+		Myset<T> answer = new Myset<T>();
+		MyLinkedList<T>.Node itr = myset.getHead();
 		while( itr != null){
 			if(a.IsMember(itr.getData()) == true){
 				answer.Insert(itr.getData());
@@ -90,6 +91,11 @@ public class Myset<T> implements MysetInterface{
 			itr = itr.getNext();
 		}
 		return answer;
+	}
+
+	public MyLinkedList<T>.Node find(T a){
+		MyLinkedList<T>.Node result = this.myset.find(a);
+		return result;
 	}
 	public String toString(){
         return myset.toString();
