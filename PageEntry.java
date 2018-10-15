8e2e872c49fc5aa08698e595d793d543e8388167
Anchit Tandon <mt6170772@iitd.ac.in>
2018-10-15 18:29:14 +0530

    Modified PageEntry.java
    Changed interface
    Added getPageIndex() implementation
    Added Contructor implementation
    Added getPageName() implementation
    Added equals() implementation
    Added toString() implementation
    Added comments

--- PageEntry.java
+++ PageEntry.java
@@ -1,14 +1,38 @@
 interface PageEntryInterface{
-	PageEntry(String pageName);
-	PageIndex getPageIndex();
+	public PageIndex getPageIndex();
 }
 
 public class PageEntry implements PageEntryInterface{
-	PageEntry(String pageName){
-		
+
+	/*
+		Stores the information related to a webpage.	 
+	 */
+	
+	private String pageName;
+	private PageIndex pageIndex;
+
+	public PageEntry(String pageName){
+		this.pageName = pageName;
+        this.pageIndex = new PageIndex();
 	}
-	PageIndex getPageIndex(){
-		
+
+	public PageIndex getPageIndex(){
+	// Returns the page index of this web-page.
+		return this.pageIndex;
 	}
+
+	public String getPageName(){
+	// Returns the page name of this web-page.
+		return this.pageName;
+	}
+
+	public boolean equals(Object obj){
+	// Two PageEntry Objects are equal if they have the same page name.
+        return this.pageName.equals(((PageEntry)obj).getPageName());
+    }
+
+    public String toString(){
+        return pageName;
+    }
 
 }
