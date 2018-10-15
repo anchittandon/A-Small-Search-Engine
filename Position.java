9cd939e196b1cd90b926cce664cf498e7e1e58e1
Anchit Tandon <mt6170772@iitd.ac.in>
2018-10-15 17:20:30 +0530

    Modified Position.java
    Added comments
    Restructured equals()

--- Position.java
+++ Position.java
@@ -4,6 +4,10 @@ interface PositionInterface{
 }
 
 public class Position implements PositionInterface {
+
+	/*
+	 Represents a Tuple <page, word position>
+	 */
 	
 	PageEntry page;
 	int wordposition;
@@ -14,15 +18,25 @@ public class Position implements PositionInterface {
 	}
 
 	public PageEntry getPageEntry(){
+	// Returns the PageEntry field
 		return page;
 	}
 
 	public int getWordIndex(){
+	// Returns the position of the word from 
+	// the beginning of document.
 		return wordposition;
 	}
 
 	public boolean equals(Object obj){
-        return this.page.equals(((Position)obj).page) && this.wordposition==((Position)obj).wordposition;
+	/*  For two Positions to be same,
+		they should have:
+		1. Same page
+		2. Same position of word in page
+	*/
+		boolean condition1 = this.page.equals(((Position)obj).page);
+		boolean condition2 = this.wordposition == ((Position)obj).wordposition;
+        return  condition1 && condition2 ;
     }
-    
+
 }
