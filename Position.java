8fe2de99ad3658aba4b896d7fe000c5d78cb6fc9
Anchit Tandon <mt6170772@iitd.ac.in>
2018-10-15 17:07:11 +0530

    Modified Position.java
    Changed PositionInterface
    Changed method definitions - added public specifier
    Added equals() implementation

--- Position.java
+++ Position.java
@@ -1,7 +1,6 @@
 interface PositionInterface{
-	Position(PageEntry p, int wordIndex);
-	PageEntry getPageEntry();
-	int getWordIndex();
+	public PageEntry getPageEntry();
+	public int getWordIndex();
 }
 
 public class Position implements PositionInterface {
@@ -9,16 +8,21 @@ public class Position implements PositionInterface {
 	PageEntry page;
 	int wordposition;
 
-	Position(PageEntry p, int wordIndex){
+	public Position(PageEntry p, int wordIndex){
 		page = p;
 		this.wordposition = wordIndex;
 	}
 
-	PageEntry getPageEntry(){
+	public PageEntry getPageEntry(){
 		return page;
 	}
 
-	int getWordIndex(){
+	public int getWordIndex(){
 		return wordposition;
 	}
+
+	public boolean equals(Object obj){
+        return this.page.equals(((Position)obj).page) && this.wordposition==((Position)obj).wordposition;
+    }
+    
 }
