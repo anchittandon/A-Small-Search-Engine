f7607d2e0d5d589379ba1177c367b6a85603de36
Anchit Tandon <mt6170772@iitd.ac.in>
2018-10-25 22:25:03 +0530

    Added SearchResult Implementation

---
+++ SearchResult.java
@@ -0,0 +1,45 @@
+interface SearchResultInterface{
+    public PageEntry getPageEntry();
+    public float getRelevance();
+    public int compareTo(SearchResult obj);
+}
+
+public class SearchResult implements Comparable<SearchResult>{
+
+    private PageEntry page;
+    private float relevance;
+
+    public SearchResult(PageEntry p, float r){
+        page = p;
+        relevance = r;
+    }
+
+    public PageEntry getPageEntry(){
+    // Returns the page of the Search Result
+        return page;
+    }
+
+    public float getRelevance(){
+    // Returns the relevance of the result
+        return relevance;
+    }
+
+    public int compareTo(SearchResult other){
+    // Gives the ordering between the current object and the "other" Object
+    	if( relevance > other.getRelevance() ){
+    		return 1;
+    	}
+    	else if( relevance < other.getRelevance() ){
+    		return -1;
+    	}
+        else{
+        	return 0;
+		}
+    }
+
+    public String toString(){
+    // Represents the data of the search result
+    	String result = "Relevance "+relevance+" of page "+page.toString();
+        return result;
+    }
+}
